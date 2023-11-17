4f0a05370bdd1a7beb45c2150b00df38f1716e74
Chandra583 <chandrashekhargawda2000@gmail.com.com>
2023-11-18 00:08:57 +0530

    just

--- 01.BasicJS/app.js
+++ 01.BasicJS/app.js
@@ -1,27 +1,27 @@
-//variables
-// (reseverd keyword ) (variable name ) (type of value)
+variables
+(reseverd keyword ) (variable name ) (type of value)
 
-//declared variable
-// let x;
+declared variable
+let x;
 
-//assing value
+assing value
 
-// let Name = "chandrashekar  ";
-// let whatDoYouWannaBecomeinYourLife = "coder";
-// let gender = "male";
-// let twiter = "chandra583";
-// console.log(
-//   ` my twitter name is ${twiter},my gender is ${gender} , i wanna become in my life ${whatDoYouWannaBecomeinYourLife} ,my name ${Name}`
-// );
+let Name = "chandrashekar  ";
+let whatDoYouWannaBecomeinYourLife = "coder";
+let gender = "male";
+let twiter = "chandra583";
+console.log(
+  ` my twitter name is ${twiter},my gender is ${gender} , i wanna become in my life ${whatDoYouWannaBecomeinYourLife} ,my name ${Name}`
+);
 
-// let a = 15;
-// let b = 10;
-// console.log(a + b);
-// console.log(a - b);
-// console.log(a * b);
-// console.log(a / b);
-// console.log(a % b);
-// console.log(a ^ b);
+let a = 15;
+let b = 10;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ^ b);
 
 let isJsProgramminganguage = true;
 let isJsHard = false;
@@ -30,3 +30,5 @@ console.log(`Is JavaScript hard to learn? ${isJsHard}`);
 let favNumb = 18;
 let add = favNumb + undefined;
 console.log(add);
+
+
