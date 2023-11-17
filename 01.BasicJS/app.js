6587000269a77c00ce72ae74348e413e0fef9dbd
Chandra583 <chandrashekhargawda2000@gmail.com.com>
2023-11-17 11:46:06 +0530

    first commit

---
+++ 01.BasicJS/app.js
@@ -0,0 +1,32 @@
+//variables
+// (reseverd keyword ) (variable name ) (type of value)
+
+//declared variable
+// let x;
+
+//assing value
+
+// let Name = "chandrashekar  ";
+// let whatDoYouWannaBecomeinYourLife = "coder";
+// let gender = "male";
+// let twiter = "chandra583";
+// console.log(
+//   ` my twitter name is ${twiter},my gender is ${gender} , i wanna become in my life ${whatDoYouWannaBecomeinYourLife} ,my name ${Name}`
+// );
+
+// let a = 15;
+// let b = 10;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ^ b);
+
+let isJsProgramminganguage = true;
+let isJsHard = false;
+console.log(`Is JavaScript programming language? ${isJsProgramminganguage}`);
+console.log(`Is JavaScript hard to learn? ${isJsHard}`);
+let favNumb = 18;
+let add = favNumb + undefined;
+console.log(add);
