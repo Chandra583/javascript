54a1531edceb7c1aab0ff2242c8a17329c49571d
Chandra583 <chandrashekhargawda2000@gmail.com.com>
2023-11-19 01:36:44 +0530

    pushss

--- 01.BasicJS/app.js
+++ 01.BasicJS/app.js
@@ -1,34 +1,52 @@
-variables
-(reseverd keyword ) (variable name ) (type of value)
+// // variables
+// // (reseverd keyword ) (variable name ) (type of value)
 
-declared variable
-let x;
+// // declared_variable
+// // let x;
 
-assing value
+// let y = 10;
+// let z = "hello";
+// let a = true;
 
-let Name = "chandrashekar  ";
-let whatDoYouWannaBecomeinYourLife = "coder";
-let gender = "male";
-let twiter = "chandra583";
-console.log(
-  ` my twitter name is ${twiter},my gender is ${gender} , i wanna become in my life ${whatDoYouWannaBecomeinYourLife} ,my name ${Name}`
-);
+// // assing value
 
-let a = 15;
-let b = 10;
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a % b);
-console.log(a ^ b);
+// // let Name = "chandrashekar  ";
+// // let whatDoYouWannaBecomeinYourLife = "coder";
+// // let gender = "male";
+// // let twiter = "chandra583";
+// // console.log(
+// //`My name is ${Name} and I want to become a ${whatDoYouWannaBecomeinYourLife}.`);
 
-let isJsProgramminganguage = true;
-let isJsHard = false;
-console.log(`Is JavaScript programming language? ${isJsProgramminganguage}`);
-console.log(`Is JavaScript hard to learn? ${isJsHard}`);
-let favNumb = 18;
-let add = favNumb + undefined;
-console.log(add);
+// // let a = 15;
+// // let b = 10;
+// // console.log(a + b);
+// // console.log(a - b);
+// // console.log(a * b);
+// // console.log(a / b);
+// // console.log(a % b);
+// // console.log(a ^ b);
 
+// // let isJsProgrammiglanguage = true;
+// /
+// // console.log(`Is JavaScript programming language? ${isJsProgramminganguage}`);
+// // console.log(`Is JavaScript hard to learn? ${isJsHard}`);
+// // let favNumb = 18;
+// // let add = favNumb + undefined;
+// // console.log(add);
 
+//strings
+
+// let isJsHard = "false";
+// console.log(isJsHard.split(""));
+
+let favActorLastName = "FA.";
+// let favActorFristName = "Chandra";
+// let fullName = favActorLastName.concat(favActorFristName);
+// let uppercase = fullName.toUpperCase();
+// console.log(fullName);
+// console.log(uppercase);
+// let message = `My fav actor is ${fullName} he is good character `.toUpperCase();
+// console.log(message);
+// //apend this"hi is best actor in the world"  message to message variable
+// message += "HI IS BEST ACTOR IN THE WORLD";
+// console.log(message);
