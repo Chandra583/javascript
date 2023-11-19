12bb4c5737c79c664322a669a944681f2aa9202e
Chandra583 <chandrashekhargawda2000@gmail.com>
2023-11-20 03:43:03 +0530

    sas

--- 02_controlflow/app.js
+++ 02_controlflow/app.js
@@ -1,149 +1,149 @@
-// let pass = prompt("please enter your passwordd");
-// //convert pass variable in sring to number
-// let passWord = parseInt(pass);
-// if (passWord === 124) {
-//   console.log("Password is correct");
-// } else if (passWord <= 124) {
-//   console.log("Password is short");
-// } else if (passWord >= 124) {
-//   console.log("Password is long");
-// } else {
-//   console.log("Error in password");
-// }
+let pass = prompt("please enter your passwordd");
+//convert pass variable in sring to number
+let passWord = parseInt(pass);
+if (passWord === 124) {
+  console.log("Password is correct");
+} else if (passWord <= 124) {
+  console.log("Password is short");
+} else if (passWord >= 124) {
+  console.log("Password is long");
+} else {
+  console.log("Error in password");
+}
 
-// let x = prompt("to what do to");
-// let y = parseInt(x);
+let x = prompt("to what do to");
+let y = parseInt(x);
 
-// switch (y) {
-//   case 0:
-//     {
-//       alert("on");
-//     }
-//     break;
-//   case 1:
-//     {
-//       alert("off");
-//     }
-//     break;
-//   default: {
-//     alert("error");
-//   }
-// }
+switch (y) {
+  case 0:
+    {
+      alert("on");
+    }
+    break;
+  case 1:
+    {
+      alert("off");
+    }
+    break;
+  default: {
+    alert("error");
+  }
+}
 
-//swith casessss
+swith casessss
 
-// let fruit = prompt("Enter your day fruit name ");
-// switch (fruit) {
-//   case "Banana":
-//     console.log("Banana is good ");
-//     break;
-//   case "Orange":
-//     console.log("I'm not fan of Orange");
-//     break;
-//   case "apple":
-//     console.log("one apple keeps you away from doctor hahaha");
-//     break;
-//   default:
-//     console.log("Sorry!! i never heard of that fruit");
-// }
+let fruit = prompt("Enter your day fruit name ");
+switch (fruit) {
+  case "Banana":
+    console.log("Banana is good ");
+    break;
+  case "Orange":
+    console.log("I'm not fan of Orange");
+    break;
+  case "apple":
+    console.log("one apple keeps you away from doctor hahaha");
+    break;
+  default:
+    console.log("Sorry!! i never heard of that fruit");
+}
 
-// for (let i = 1; i <= 1000; i++) {
-//   console.log("Happy birthday to me", i);
-// }
+for (let i = 1; i <= 1000; i++) {
+  console.log("Happy birthday to me", i);
+}
 
-//Arraays
-// let favSingers = ["Honey", "singh", "arjith"];
-// console.log(favSingers[0]);
-// let favNumb = [54, 55, 89, 18];
-// let mixedArr = ["str", ["other", "jsj"], 123, false];
-// console.log(mixedArr[1][1]);
+Arraays
+let favSingers = ["Honey", "singh", "arjith"];
+console.log(favSingers[0]);
+let favNumb = [54, 55, 89, 18];
+let mixedArr = ["str", ["other", "jsj"], 123, false];
+console.log(mixedArr[1][1]);
 
-//object
+object
 
-// const car = {
-//   type: "mahindra",
-//   model: 2023,
-//   color: "red",
-// };
-// console.log(typeof car);
+const car = {
+  type: "mahindra",
+  model: 2023,
+  color: "red",
+};
+console.log(typeof car);
 
-// car.type = "Toyota";
-// car.wheels = 4;
-// console.log(car);
+car.type = "Toyota";
+car.wheels = 4;
+console.log(car);
 
+let Name = "Vignesh sir";
+function sayHello(Name) {
+  console.log(`hello ${Name}`);
+}
 // let Name = "Vignesh sir";
-// function sayHello(Name) {
-//   console.log(`hello ${Name}`);
-// }
-// // let Name = "Vignesh sir";
 
-// sayHello("vignesh");
-// sayHello("viggu");
-// sayHello("ganesh");
+sayHello("vignesh");
+sayHello("viggu");
+sayHello("ganesh");
 
-// //retun keyword in function
-// function addNumber() {
-//   return 6 + 7;
-// }
-// console.log(addNumber());
+//retun keyword in function
+function addNumber() {
+  return 6 + 7;
+}
+console.log(addNumber());
 
-// function myFun(a, b
+function myFun(a, b
 
-// function birthday(Name, dob) {
-//   console.log(`my name is ${Name}`);
-// }
-// function dob() {
-//   console.log("And my birthaday in 11/8/2000");
-// }
+function birthday(Name, dob) {
+  console.log(`my name is ${Name}`);
+}
+function dob() {
+  console.log("And my birthaday in 11/8/2000");
+}
 
-// birthday("Vignesh", dob());
+birthday("Vignesh", dob());
 
-// function showCallfunc(fn) {
-//   let value = 25;
-//   fn(value);
-// }
+function showCallfunc(fn) {
+  let value = 25;
+  fn(value);
+}
 
-// showCallfunc(function (value) {
-//   console.log(value * 2);
-// });
+showCallfunc(function (value) {
+  console.log(value * 2);
+});
 
-// function greet(name, cb, ls) {
-//   console.log(`hi my name ${name}`);
-//   cb();
-//   ls();
-// }
+function greet(name, cb, ls) {
+  console.log(`hi my name ${name}`);
+  cb();
+  ls();
+}
 
-// function callme() {
-//   console.log("I am a call back function");
-// }
-// function secoundCallme() {
-//   console.log("I am second call back function");
-// }
+function callme() {
+  console.log("I am a call back function");
+}
+function secoundCallme() {
+  console.log("I am second call back function");
+}
 
-// greet("Vignesh", callme, secoundCallme);
+greet("Vignesh", callme, secoundCallme);
 
-// function showCallfunc(fn) {
-//   let value = 10;
-//   fn(value);
-// }
-// showCallfunc(function (value) {
-//   console.log(value * 3);
-// });
+function showCallfunc(fn) {
+  let value = 10;
+  fn(value);
+}
+showCallfunc(function (value) {
+  console.log(value * 3);
+});
 
-// const oldSeason = (year) => {
-//   console.log(`Welocome to New season of ${year} :)`);
-// };
-// oldSeason(2023);
+const oldSeason = (year) => {
+  console.log(`Welocome to New season of ${year} :)`);
+};
+oldSeason(2023);
 
-// const greet = (name) => {
-//   return `Hello World ${name}`;
-// };
-// console.log(greet("vignesh"));
+const greet = (name) => {
+  return `Hello World ${name}`;
+};
+console.log(greet("vignesh"));
 
-// const add = (x, y, z) => {
-//   return x + y + z;
-// };
-// console.log(add(4, 6, 8));
+const add = (x, y, z) => {
+  return x + y + z;
+};
+console.log(add(4, 6, 8));
 
 function student(name, ID, year, section) {
   return { name, ID, year, section };
