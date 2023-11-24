fb8c03dd94c58511fd26e7a1068489ef858d48b6
Chandra583 <chandrashekhargawda2000@gmail.com>
2023-11-24 22:09:35 +0530

    dom manupulation

--- DOM/app.js
+++ DOM/app.js
@@ -1,1 +1,9 @@
+console.log(document.getElementsByTagName("h1"));
+console.log(document.getElementsByTagName("h1").length);
+console.log(document.getElementById("id-1"));
+console.log(document.getElementsByClassName("cls"));
 
+console.log(document.querySelectorAll("li"));
+
+//storing as a variable
+const h1 = document.getElementsByTagName("h1");
