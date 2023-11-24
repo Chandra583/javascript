2a0cf00741ec05e82c0a7489ea912bf26361f89c
Chandra583 <chandrashekhargawda2000@gmail.com>
2023-11-24 22:26:03 +0530

    Accessing element by tag  name

--- DOM/app.js
+++ DOM/app.js
@@ -1,9 +1,16 @@
-console.log(document.getElementsByTagName("h1"));
-console.log(document.getElementsByTagName("h1").length);
-console.log(document.getElementById("id-1"));
-console.log(document.getElementsByClassName("cls"));
+// 1. Select element h4 which holds text of "Red" by using getElementsByTagName
+// 2. Select div with the class of "green" which holds text (Green) by using getElementsByTagName
+// 3. Select div with the ID of blue which holds the text "Blue" by using getElementsByClassName
+// 4. Select div which has the class & Id of "yello" by using querySelector()
+// 5. Select all the elements which has the class of "teal" by using querySelectorAll
 
-console.log(document.querySelectorAll("li"));
-
-//storing as a variable
-const h1 = document.getElementsByTagName("h1");
+const red = document.getElementsByTagName("h4");
+console.log(red);
+const green = document.getElementsByClassName("green");
+console.log(green);
+const blue = document.getElementById("blue");
+console.log(blue);
+const yellow = document.querySelector("#yellow");
+console.log(yellow);
+const teal = document.querySelectorAll(".teal");
+console.log(teal);
