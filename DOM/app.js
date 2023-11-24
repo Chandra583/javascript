af78c176ba5daf5ac24a18c6787251ec229fde56
Chandra583 <chandrashekhargawda2000@gmail.com>
2023-11-25 03:02:08 +0530

    innreTex,content,

--- DOM/app.js
+++ DOM/app.js
@@ -1,16 +1,10 @@
-// 1. Select element h4 which holds text of "Red" by using getElementsByTagName
-// 2. Select div with the class of "green" which holds text (Green) by using getElementsByTagName
-// 3. Select div with the ID of blue which holds the text "Blue" by using getElementsByClassName
-// 4. Select div which has the class & Id of "yello" by using querySelector()
-// 5. Select all the elements which has the class of "teal" by using querySelectorAll
+// 1. Check the innerText of "first" element
+const ine1 = document.querySelector(".second");
+console.log(ine1.innerText);
+// 2. Check the textContent of "second" element
+const ine2 = document.querySelector(".second");
+console.log(ine2.textContent);
+// 3. Check the innerHTML of "third" element
 
-const red = document.getElementsByTagName("h4");
-console.log(red);
-const green = document.getElementsByClassName("green");
-console.log(green);
-const blue = document.getElementById("blue");
-console.log(blue);
-const yellow = document.querySelector("#yellow");
-console.log(yellow);
-const teal = document.querySelectorAll(".teal");
-console.log(teal);
+const ine3 = document.querySelector(".third");
+console.log(ine3.innerText);
